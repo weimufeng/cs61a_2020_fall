82ccdb5d15b9793ee386a811c685b478da7fc8df
Awei0418 <61162791+Awei0418@users.noreply.github.com>
2020-08-31 21:31:15 -0700

    Add files via upload

---
+++ lab/lab01.py
@@ -0,0 +1,75 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    product = n
+    if k == 0:
+        return 1
+    else:
+        while k > 1:
+            product *= (n - 1)
+            n -= 1
+            k -= 1
+        
+        return product
+
+
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    sum = 0
+    while y > 0:
+        sum += y % 10
+        y //= 10
+    
+    return sum
+
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    while n > 0:
+        if n % 10 == 8 and (n // 10) % 10 == 8:
+            return True
+        n //= 10        
+    return False
+
+
+
+
