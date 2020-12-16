4d9d35182490c8ef09d7b0ec48f556ceba9263e2
Mufeng Wei <mufeng_wei@berkeley.edu>
2020-12-16 14:46:06 -0800

    Add files via upload

---
+++ labs/lab07_solution.py
@@ -0,0 +1,69 @@
+def naturals():
+    """A generator function that yields the infinite sequence of natural
+    numbers, starting at 1.
+
+    >>> m = naturals()
+    >>> type(m)
+    <class 'generator'>
+    >>> [next(m) for _ in range(10)]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+    i = 1
+    while True:
+        yield i
+        i += 1
+
+
+def scale(it, multiplier):
+    """Yield elements of the iterable it scaled by a number multiplier.
+
+    >>> m = scale([1, 5, 2], 5)
+    >>> type(m)
+    <class 'generator'>
+    >>> list(m)
+    [5, 25, 10]
+
+    >>> m = scale(naturals(), 2)
+    >>> [next(m) for _ in range(5)]
+    [2, 4, 6, 8, 10]
+    """
+    for elem in it:
+        yield elem * multiplier
+
+# Alternate solution
+def scale_alt(it, multiplier):
+    yield from map(lambda x: x*multiplier, it)
+
+
+def hailstone(n):
+    """
+    >>> for num in hailstone(10):
+    ...     print(num)
+    ...
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    """
+    while n > 1:
+        yield n
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = n * 3 + 1
+    yield n
+
+# Alternate Solution
+
+def hailstone_alt(n):
+    yield n
+    if n > 1:
+        if n % 2 == 0:
+            yield from hailstone_alt(n // 2)
+        else:
+            yield from hailstone_alt(n * 3 + 1)
+
+
