50319b8aacf1ca4f4d9a200b99a6fac18d3bf491
Mufeng Wei <mufeng_wei@berkeley.edu>
2020-12-16 14:40:35 -0800

    Add files via upload

---
+++ hws/hw09_solution.sql
@@ -0,0 +1,48 @@
+CREATE TABLE parents AS
+  SELECT "abraham" AS parent, "barack" AS child UNION
+  SELECT "abraham"          , "clinton"         UNION
+  SELECT "delano"           , "herbert"         UNION
+  SELECT "fillmore"         , "abraham"         UNION
+  SELECT "fillmore"         , "delano"          UNION
+  SELECT "fillmore"         , "grover"          UNION
+  SELECT "eisenhower"       , "fillmore";
+
+CREATE TABLE dogs AS
+  SELECT "abraham" AS name, "long" AS fur, 26 AS height UNION
+  SELECT "barack"         , "short"      , 52           UNION
+  SELECT "clinton"        , "long"       , 47           UNION
+  SELECT "delano"         , "long"       , 46           UNION
+  SELECT "eisenhower"     , "short"      , 35           UNION
+  SELECT "fillmore"       , "curly"      , 32           UNION
+  SELECT "grover"         , "short"      , 28           UNION
+  SELECT "herbert"        , "curly"      , 31;
+
+CREATE TABLE sizes AS
+  SELECT "toy" AS size, 24 AS min, 28 AS max UNION
+  SELECT "mini"       , 28       , 35        UNION
+  SELECT "medium"     , 35       , 45        UNION
+  SELECT "standard"   , 45       , 60;
+
+
+-- The size of each dog
+CREATE TABLE size_of_dogs AS
+  SELECT name, size FROM dogs, sizes
+    WHERE height > min AND height <= max;
+
+
+-- All dogs with parents ordered by decreasing height of their parent
+CREATE TABLE by_parent_height AS
+  SELECT child FROM parents, dogs WHERE name = parent ORDER BY -height;
+
+
+-- Filling out this helper table is optional
+CREATE TABLE siblings AS
+  SELECT a.child AS first, b.child AS second FROM parents AS a, parents AS b
+    WHERE a.parent = b.parent AND a.child < b.child;
+
+-- Sentences about siblings that are the same size
+CREATE TABLE sentences AS
+  SELECT "The two siblings, " || first || " plus " || second || " have the same size: " || a.size
+    FROM siblings, size_of_dogs AS a, size_of_dogs AS b
+    WHERE a.size = b.size AND a.name = first AND b.name = second;
+
